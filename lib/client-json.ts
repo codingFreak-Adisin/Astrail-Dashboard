70124136d167357c0deb304a140282c1ee873d38
Aditya Sinha <70958307+codingFreak-Adisin@users.noreply.github.com>
2026-07-18 15:43:47 +0530

    Delete lib directory

--- lib/client-json.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-
-export async function readJsonResponse<T>(response: Response): Promise<T> {
-  const text = await response.text();
-  if (!text) return {} as T;
-  return JSON.parse(text) as T;
-}
--- lib/runtime/batch.ts
+++
@@ -1,7 +0,0 @@
-export async function isolateBatchItem<T>(work: () => Promise<T>, onError: (error: unknown) => T | Promise<T>) {
-  try {
-    return await work();
-  } catch (error) {
-    return await onError(error);
-  }
-}
